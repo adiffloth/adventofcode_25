564be8da426f0659e7ba481bef90b4b444d613be
Antoine Diffloth <adiffloth@gmail.com>
2025-12-01 15:23:58 -0800

    Day 1 part 1

---
+++ day_01/part_2.py
@@ -0,0 +1,14 @@
+lines = open('day_01/1.dat').read().splitlines()
+
+pos = 50
+combo = 0
+
+for line in lines:
+    dir = 1 - 2 * (line[0] == 'L')
+    amt = int(line[1:])
+    eff = amt * dir
+    pos = (pos + amt*dir) % 100
+    if pos == 0:
+        combo += 1
+
+print(combo)
